90eae2f05e8126c8166acec966644ada1ae7d2b2
Saraswathi <saraswathi.zerosoft@gmail.com>
2022-04-20 10:05:52 +0530

    update-design changes

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ViewChild, ElementRef } from "@angular/core";
 import {
   CdkDragDrop,
   moveItemInArray,
@@ -14,10 +14,24 @@ import { TodoFormComponent } from "./todo-form/todo-form.component";
   styleUrls: ["./todo.component.scss"],
 })
 export class TodoComponent implements OnInit {
+  @ViewChild("focus") focusElement: ElementRef;
   todo: any;
   done: any;
-
-  constructor(private todoService: TodoService, public dialog: MatDialog) {}
+  data: any = {
+    title: "",
+    userId: 1,
+    id: 0,
+    completed: false,
+  };
+  taskInput: boolean = false;
+  editId: number = 0;
+  toolbar: boolean = false;
+  toolbarId: any = [];
+  constructor(
+    private todoService: TodoService,
+    public dialog: MatDialog,
+    private el: ElementRef
+  ) {}
 
   ngOnInit(): void {
     this.getData();
@@ -82,7 +96,7 @@ export class TodoComponent implements OnInit {
       this.done = this.done.filter((task: any) => task.id !== taskId);
     }
   }
-  
+
   radioClick(item: any, list: any) {
     if (list == "todo") {
       this.todo = this.todo.filter((task: any) => task.id !== item.id);
@@ -108,6 +122,70 @@ export class TodoComponent implements OnInit {
         event.previousIndex,
         event.currentIndex
       );
+    }
+  }
+
+  createTaskInput(el: HTMLElement) {
+    el.scrollIntoView();
+    this.taskInput = true;
+    this.focusElement.nativeElement.focus();
+  }
+  onChange() {
+    this.data.completed = !this.data.completed;
+  }
+  removeInput() {
+    this.taskInput = false;
+    this.editId = 0;
+    this.data = {
+      title: "",
+      userId: 1,
+      id: 0,
+      completed: false,
+    };
+  }
+  addTask() {
+    this.data.id = this.todo.length + this.done.length + 1;
+    console.log(this.data);
+    this.todoService
+      .postData("todos", JSON.stringify(this.data))
+      .then((res) => {
+        let result = this.data.completed
+          ? this.done.unshift(this.data)
+          : this.todo.unshift(this.data);
+        alert("Task added !");
+        this.removeInput();
+      });
+  }
+  editInput(item: any, list: any) {
+    this.editId = item.id;
+    this.focusElement.nativeElement.focus();
+  }
+
+  onEdit(item: any, list: any) {
+    if (list == "todo") {
+      this.todo = this.todo.filter((task: any) => {
+        if (task.id === item.id) {
+          task.title = item.title;
+        }
+        return task;
+      });
+    } else {
+      this.done = this.done.filter((task: any) => {
+        if (task.id === item.id) {
+          task.title = item.title;
+        }
+        return task;
+      });
+    }
+    this.removeInput();
+    alert("Task updated !");
+  }
+  onClick(id: any) {
+    let i = this.toolbarId.indexOf(id);
+    if (i == -1) {
+      this.toolbarId.push(id);
+    } else {
+      this.toolbarId.splice(i, 1);
     }
   }
 }
